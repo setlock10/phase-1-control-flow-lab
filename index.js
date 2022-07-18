b5e0e099254ec71b25a20095e9f39eff42b9e066
John J Setlock II <johnsetlock@gmail.com>
2022-07-18 16:54:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength<=400){ return 'This one is on me!';  }
+  
+  if ((rideLength>400)&&(rideLength<2000)){ return 'That will be twenty bucks.'; }
+
+  if ((rideLength>400)&&(rideLength<2000)){ return 'That will be twenty bucks.'; }
+
+  if ((rideLength>2000)&&(rideLength<2500)){ return 'I will gladly take your thirty bucks.'; }
+
+  if (rideLength>2500){ return 'No can do.'; }
+
+  
+      
+
+
+    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  if (cityName=='NYC')
+      return 'Ok, sounds good.';
+  else
+  return 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipQuality){
   // Write your code here!
+  if (tipQuality=='generous')
+  {
+  return 'Thank you so much.';
+  }
+  if (tipQuality=='not as generous')
+  {
+  return 'Thank you.';
+  }
+
+  else 
+  return 'Bye.';
+
+
+
 }
